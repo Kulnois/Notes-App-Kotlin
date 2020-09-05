5f0323820ba3bde1c98bb5f4277f1412fb90c5a6
Kulnois <rasecmen58@hotmail.com>
2020-09-05 13:42:42 -0500

    Add view structure NoteEditorFragment add library android material

---
+++ app/src/main/java/com/kulnois/notesapp/ui/editor/NoteEditorFragment.kt
@@ -0,0 +1,21 @@
+package com.kulnois.notesapp.ui.editor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by @kulnois on 5/09/2020.
+ */
+
+class NoteEditorFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/kulnois/notesapp/ui/editor/NoteEditorViewModel.kt
@@ -0,0 +1,11 @@
+package com.kulnois.notesapp.ui.editor
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by @kulnois on 5/09/2020.
+ */
+
+class NoteEditorViewModel () : ViewModel() {
+
+}
